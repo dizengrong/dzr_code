0c5a376dd507f035b8938a296000f714a57104f7
dizengrong <dizengrong@gmail.com>
2013-08-29 11:18:21 +0800

    remove some unused file of 4399 job code

--- job_code/Sanguo_X_server/src/data/data_ai.erl
+++
@@ -1,7 +0,0 @@
--module(data_ai).
--include("common.hrl").
-
--export([get/1]).
-
-get(_) ->
-	?UNDEFINED.	
--- job_code/Sanguo_X_server/src/data/data_team.erl
+++
@@ -1,7 +0,0 @@
--module(data_team).
-
--export([get_scene_list/0]).
-
-get_scene_list()->
-	[1400].
-
