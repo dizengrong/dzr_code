9a09a6871abe150f5c2f5ff7f330749ac67218a3
dizengrong <dizengrong@gmail.com>
2013-10-07 09:01:22 +0800

    add setup scripts

---
+++ python/my_license_control/setup.py
@@ -0,0 +1,35 @@
+#-*- coding: utf8 -*-
+
+# Requires wxPython. &nbsp;This sample demonstrates:
+#
+# - single file exe using wxPython as GUI.
+
+from distutils.core import setup
+import py2exe
+import sys
+
+# If run without args, build executables, in quiet mode.
+
+if len(sys.argv) == 1:
+	sys.argv.append("py2exe")
+	sys.argv.append("-q")
+
+
+
+includes=["encodings","encodings.*"]
+setup(
+options = {"py2exe": {"compressed": 1,
+					  "optimize": 2,
+					  # "ascii": 0,
+					  "includes":includes,
+					  "dll_excludes": ["MSVCP90.dll"]
+					  }
+		  },
+zipfile = None,
+windows = [{"script":"main.py"}],
+data_files = [
+
+    	     ]
+)
+
+############################################################
