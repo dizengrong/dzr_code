adef7fe423a91df01285e37ccf70289fbbf8ce45
dizengrong <dizengrong@gmail.com>
2014-03-10 17:59:58 +0800

    update depot

--- python/depot/SearchResutlPanel.py
+++ python/depot/SearchResutlPanel.py
@@ -15,6 +15,12 @@ from matplotlib.font_manager import FontProperties
 
 # font = FontProperties(fname=r"c:\\windows\\fonts\\simsun.ttc", size=14) 
 
+def InitPrices():
+	deal_prices = {}
+	for category in models.ALL_PRODUCT_TYPE2.keys():
+		deal_prices[category] = 0
+	return deal_prices
+
 class MySearchResutlPanel(SearchResutlPanel):
 	def __init__(self, parent, begin_date, end_date, search_type, search_args = None):
 		super(MySearchResutlPanel, self).__init__(parent)
@@ -29,12 +35,14 @@ class MySearchResutlPanel(SearchResutlPanel):
 			search_cond = u"从%s到%s之间，产品%s中型号为%s的所有交易记录" % \
 							(begin_date, end_date, category, u"、".join(type_list))
 		elif search_type == SEARCH_TYPE_BUYER:
-			search_cond = u"todo"
-		else:
-			search_cond = "todo"
+			buyer       = search_args
+			search_cond = u"从%s到%s之间，与买家%s的所有交易记录" % (begin_date, end_date, buyer)
+
 		self.m_staticText14.SetLabel(search_cond)
 
 		gui_util.InitSellRecordTab(self.m_grid2)
+		self.search_type    = search_type
+		self.search_args    = search_args
 		self.search_results = self.DoSearch(begin_date, end_date, search_type, search_args)
 
 		row_count = 0
@@ -63,35 +71,24 @@ class MySearchResutlPanel(SearchResutlPanel):
 	def DoSearch(self, begin_date, end_date, search_type, search_args):
 		if search_type == SEARCH_TYPE_TOTAL:
 			return db.QuerySellsByDate(begin_date, end_date)
+		elif search_type == SEARCH_TYPE_PRODUCT:
+			(category, type_list) = search_args
+			return db.QuerySellsByProductType(begin_date, end_date, category, type_list)
+		elif search_type == SEARCH_TYPE_BUYER:
+			buyer = search_args
+			return db.QuerySellsWithBuyer(begin_date, end_date, buyer)
 
 	def OnGenerateReport(self, event):
-		m_panel28 = wx.Panel( self.m_notebook2, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL )
-		self.m_notebook2.AddPage( m_panel28, u"报表", True )
-
+		self.m_notebook2.AddPage( self.m_panel28, u"报表", True )
 		panel        = m_panel28
 		panel.SetBackgroundColour(wx.NamedColour("WHITE"))
 		
-		panel.figure = Figure()
-		panel.axes   = panel.figure.add_subplot(111)
-		
-		deal_prices = {}
-		for category in models.ALL_PRODUCT_TYPE2.keys():
-			deal_prices[category] = 0
-		for sell_rec in self.search_results.values():
-			deal_prices[sell_rec.product_class] = deal_prices[sell_rec.product_class] + float(sell_rec.deal_price)
-		self.labels = []
-		self.sizes  = []
-		self.colors = []
-		for category in deal_prices.keys():
-			self.labels.append(models.ALL_PRODUCT_TYPE2[category])
-			self.sizes.append(deal_prices[category])
-			self.colors.append(models.PRODUCT_COLORS[category])
-		# explode      = (0, 0, 0, 0) # only "explode" the 2nd slice (i.e. 'Hogs')
-		panel.axes.pie(self.sizes, labels=self.labels, colors=self.colors, 
-					   autopct='%1.1f%%', shadow=True, startangle=90)
-		# Set aspect ratio to be equal so that pie is drawn as a circle.
-		panel.axes.axis('equal')
-		panel.canvas = FigureCanvas(panel, -1, panel.figure)
+		if self.search_type == SEARCH_TYPE_TOTAL:
+			self.DrawTotalReport(panel)
+		elif search_type == SEARCH_TYPE_PRODUCT:
+			self.DrawProductReport(panel)
+		elif search_type == SEARCH_TYPE_BUYER:
+			self.DrawSelleWithBuyer(panel)
 
 		panel.sizer = wx.BoxSizer(wx.VERTICAL)
 		panel.sizer.Add(panel.canvas, 1, wx.ALL|wx.EXPAND)
@@ -100,3 +97,54 @@ class MySearchResutlPanel(SearchResutlPanel):
 
 		self.Layout()
 
+	def DrawTotalReport(self, panel):
+		deal_prices = InitPrices()
+		for sell_rec in self.search_results.values():
+			deal_prices[sell_rec.product_class] = deal_prices[sell_rec.product_class] + float(sell_rec.deal_price)
+
+		labels = []
+		sizes  = []
+		colors = []
+		for category in deal_prices.keys():
+			labels.append(models.ALL_PRODUCT_TYPE2[category])
+			sizes.append(deal_prices[category])
+			colors.append(models.PRODUCT_COLORS[category])
+
+		panel.figure = Figure()
+		panel.axes   = panel.figure.add_subplot(111)
+		# explode      = (0, 0, 0, 0) # only "explode" the 2nd slice (i.e. 'Hogs')
+		panel.axes.pie(sizes, labels=labels, colors=colors, autopct='%1.1f%%', shadow=True, startangle=90)
+		# Set aspect ratio to be equal so that pie is drawn as a circle.
+		panel.axes.axis('equal')
+		panel.canvas = FigureCanvas(panel, -1, panel.figure)
+
+
+	def DrawProductReport(self, panel):
+		"""产品相关查询报表"""
+		(category, type_list) = self.search_args
+		deal_prices = {}
+		for t in type_list:
+			deal_prices[t] = 0
+		for sell_rec in self.search_results:
+			deal_prices[sell_rec.product_type] = deal_prices[sell_rec.product_type] + float(sell_rec.deal_price)
+			
+		labels = []
+		sizes  = []
+		colors = []
+		index  = 0
+		for product_type in deal_prices.keys():
+			labels.append(product_type)
+			sizes.append(deal_prices[product_type])
+			colors.append(common.COLOUR_TAB[index])
+			if index < len(common.COLOUR_TAB) - 1:
+				index = index + 1
+
+		panel.figure = Figure()
+		panel.axes   = panel.figure.add_subplot(111)
+		panel.axes.pie(sizes, labels=labels, colors=colors, autopct='%1.1f%%', shadow=True, startangle=90)
+		panel.axes.axis('equal')
+		panel.canvas = FigureCanvas(panel, -1, panel.figure)
+
+	def DrawSelleWithBuyer(self, panel):
+		"""与某买家在某段时间内的所有交易记录的报表"""
+		
--- python/depot/db.py
+++ python/depot/db.py
@@ -20,7 +20,6 @@ SQL_UPDATE_SELL = "update sell_record set product_class = %s, product_type = '%s
 				  "paid = %s, deal_date = '%s' where uid = %s" \
 # 查询与某买家的历史交易
 SQL_QUERY_SELLS_BY_NAME = SQL_SELECT_SELLS + " and buyer.buyer_name = '%s' order by sell_record.uid"
-
 # 查询所有的产品信息
 SQL_ALL_PRODUCT = "select class, type, length, width, height, per_weight, price from product order by class, type"
 # 插入产品数据
@@ -37,8 +36,25 @@ SQL_ALL_BUYERS = "select uid, buyer_name, phone1, phone2, phone3, email from buy
 SQL_UPDATE_BUYER = "update buyer set buyer_name = '%s', phone1 = '%s', phone2 = '%s', phone3 = '%s', email = '%s' where uid = %s"
 # 插入买家信息
 SQL_INSERT_BUYER = "insert into buyer (buyer_name, phone1, phone2, phone3, email) VALUES ('%s', '%s', '%s', '%s', '%s')"
+# 获取所有售出记录的成交价的总和
+SQL_TOTAL_DEAL_PRICE = "select sum(deal_price) as total from sell_record"
+# 获取所有售出记录的已支付款的总和
+SQL_TOTAL_PAID = "select sum(paid) as total from sell_record"
+
 db_conn = sqlite3.connect("depot.sqlite")
 
+
+def GetTotalDealPrice():
+	"""获取所有售出记录的成交价的总和"""
+	db_cur = db_conn.cursor()
+	db_cur.execute(SQL_TOTAL_DEAL_PRICE)
+	return db_cur.next()[0]
+
+def GetTotalPaid():
+	"""获取所有售出记录的已支付款的总和"""
+	db_cur = db_conn.cursor()
+	db_cur.execute(SQL_TOTAL_PAID)
+	return db_cur.next()[0]
 
 def GetAllSellRecords():
 	"""获取所有的售出记录"""
@@ -64,6 +80,30 @@ def GetHistorySellsByBuyerName(buyer_name):
 def QuerySellsByDate(begin_date, end_date):
 	db_cur = db_conn.cursor()
 	sql    = SQL_SELECT_SELLS + " and deal_date between '%s' and '%s'" % (begin_date, end_date)
+	db_cur.execute(sql)
+	all_sells = {}
+	for row in db_cur:
+		rec = SqlDatas2SellRecord(row)
+		all_sells[rec.uid]  = rec
+	return all_sells
+
+def QuerySellsByProductType(begin_date, end_date, category, type_list):
+	db_cur = db_conn.cursor()
+	tmp = ", ".join(["'" + t + "'" for t in type_list])
+	sql    = SQL_SELECT_SELLS + " and deal_date between '%s' and '%s'" % (begin_date, end_date) + \
+			 " and product_class = %s and product_type in (%s)" % tmp
+	db_cur.execute(sql)
+	all_sells = {}
+	for row in db_cur:
+		rec = SqlDatas2SellRecord(row)
+		all_sells[rec.uid]  = rec
+	return all_sells
+
+def QuerySellsWithBuyer(begin_date, end_date, buyer_name):
+	"""查询与某买家在某段时间内的所有交易记录"""
+	db_cur = db_conn.cursor()
+	sql    = SQL_SELECT_SELLS + " and buyer_name = '%s' " % buyer_name + \
+			 " and deal_date between '%s' and '%s'" % (begin_date, end_date)
 	db_cur.execute(sql)
 	all_sells = {}
 	for row in db_cur:
